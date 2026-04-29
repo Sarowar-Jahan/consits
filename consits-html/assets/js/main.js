b87647fe524326857a102047a2cf85ff17c85180
Sarowar-Jahan <mdsarowarjahan3@gmail.com>
2026-04-29 17:38:34 +0600

    update

--- consits-html/assets/js/main.js
+++ consits-html/assets/js/main.js
@@ -774,6 +774,21 @@
     });
   }
 
+  // text-slider-6 active 
+  if (document.querySelectorAll(".text-slider-6-active").length > 0) {
+    var text_slider_6_active = new Swiper(".text-slider-6-active", {
+      slidesPerView: 'auto',
+      loop: true,
+      autoplay: true,
+      spaceBetween: 0,
+      speed: 20000,
+      allowTouchMove: false,
+      autoplay: {
+        delay: 1,
+      },
+    });
+  }
+
   // price tab switcher
   const toggle_switcher = function () {
     let $scope = $(".ui-toggle_switcher");
